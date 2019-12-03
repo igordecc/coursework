0c3f2645860943fca8baf14a6a43fe3401c4d979
igordecc <Igordecc@yandex.ru>
2019-12-03 19:01:56 +0400

    Merg react_html_canvas from practice with this proj

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import App from './App.js';
+import './index.css';
+
+//single point to load all app scripts
 
 render(<App />, document.getElementById("root"));
