0d26400af612e3f34344b72c18d7bd691bac3403
igordecc <Igordecc@yandex.ru>
2019-12-16 01:33:49 +0400

    Add color list. Draw function now accept color

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -9,12 +9,13 @@ const DataURL = `http://localhost:5000/`
 const SCALE = 0.3
 const OFFSET = 80
 
+
 var oscillators_number = 0
 var group_number = 0
 var screen_lines = []
 
 // canvas draw functions
-function draw_circle(ctx, location) {
+function draw_circle(ctx, location, colour) {
   ctx.save();
   ctx.fillStyle = 'rgb(255, 51, 204)'
   ctx.shadowColor = 'dodgeblue'
@@ -23,7 +24,8 @@ function draw_circle(ctx, location) {
   ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET)
   ctx.beginPath();
   ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(255, 51, 204)';
+  //ctx.fillStyle = 'rgb(255, 51, 204)';
+  ctx.fillStyle = colour;
   ctx.fill();
   ctx.stroke();
   ctx.restore();
@@ -51,22 +53,57 @@ function usePersistentState(init, itemName='') {
   return [value, setValue]
 }
 
-// set canvas hook 
+// ----------hooks-----------
 // hook is a data managing function
-function usePersistentCanvas() {
+function usePersistentCanvas(data) {
   const [locations, setLocations] = usePersistentState([], 'draw-app')
-
   const canvasRef = React.useRef(null)
+  var [colorList, setColorList] = usePersistentState([],'color-list')
 
   // update canvas
   React.useEffect(() => {
+    // delay
+    let timeout = 1000
+    setTimeout(() => {}, timeout);
+
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-    locations.forEach(location => draw_circle(ctx, location))
-    screen_lines.forEach(line => draw_line(ctx, line))
+    
+    // create default color list
+    function create_default_color_list(){
+      let colour_list = []
+      for (let location in locations) {
+        let new_colour = data.phase_vector[0][location]*50
+        colour_list.push(`hsl(300, 100%, 50%)`) //max 360
+      }  
+      return colour_list
+    }
+
+    // zip
+    function zip_locations_and_color_list(colour_list){
+      let zipped = []
+      for (let location in locations) {
+        zipped.push([locations[location], colour_list[location]])
+      }
+      return zipped
+    }
+
+    function draw_all(zipped, screen_lines){
+      zipped.forEach((l_and_c) => draw_circle(ctx, l_and_c[0], l_and_c[1]))
+      screen_lines.forEach(line => draw_line(ctx, line))
+    }
+
+    let colour_list = create_default_color_list()
+    let zipped = zip_locations_and_color_list(colour_list)
+    draw_all(zipped, screen_lines)
+
+    // dont use setColorList(colour_list)
+    // set up color other way or reed how to work useEffect
   })
-  return [locations, setLocations, canvasRef]
+  
+  
+  return [locations, setLocations, canvasRef, colorList, setColorList]
 }
 
 // set data hook
@@ -76,21 +113,14 @@ function usePersistentData(init){
   return [data, setData]
 }
 
-// set locations hook
-function usePersistentRandomLocations(init) {
-  const [locations, setLocations] = usePersistentState(init, 'osc-locations')
-  
-  return [locations, setLocations]
-}
+// -------------------------------
 
 // Application render function
 function App() {
   // states
-  const [locations, setLocations, canvasRef] = usePersistentCanvas([]);
   const [data, setData] = usePersistentData({});
+  const [locations, setLocations, canvasRef, colorList, setColorList] = usePersistentCanvas(data);
   
-  const [oscNumber, setOscNumber] = React.useState(null);
-  const [randomLocations, setRandomLocations] = usePersistentRandomLocations([]);
 
   // handlers
   function handleCanvasClick (e) {
@@ -106,64 +136,101 @@ function App() {
     setLocations(locations.slice(0,-1))
   }
 
+
   function handleReloadOscillatorsData (){  
-    // connection to server
-    fetch(DataURL).           
-    then(result => result.json()).
-    then(e => {
-      setData(e);
-    }).
-    catch(error => console.log(error))
-    console.log(data)
+    // reload everything - all app
 
-    // main parameters
-    oscillators_number = _.size(data.Aij[0])
-    group_number = _.size(data.community_list)
-
-    // dividing screen acording to group size
-    // var screen_lines = []  // at the constants
-    let vertical_line = 0
-    screen_lines = []
-    for (let i=0; i < group_number; i++) {
-      let community_size = _.size(data.community_list[i])
-      vertical_line += window.innerWidth * (community_size / oscillators_number) 
-      screen_lines.push( vertical_line )
+    function fetch_data() {
+      // connection to server
+      fetch(DataURL).           
+      then(result => result.json()).
+      then(e => {
+        setData(e);
+      }).
+      catch(error => console.log(error))
+      //console.log(data)
     }
-    console.log("screen lines:", screen_lines)
     
-    // random normal distribution [0 ; 1]
-    function rand_normal() {
-      var u = 0, v = 0;
-      while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-      while(v === 0) v = Math.random();
-      let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-      num = num / 10.0 + 0.5; // Translate to 0 -> 1
-      if (num > 1 || num < 0) return rand_normal(); // resample between 0 and 1
-      return num;
+    function define_data_params(){
+        // main parameters
+      oscillators_number = _.size(data.Aij[0])
+      group_number = _.size(data.community_list)
+      //console.log(oscillators_number, group_number)
     }
 
-    var calculate_oscillators = function () {
-      // for let _line in screen_lines
-      //for (let line_number=0; line_number < _.size(screen_lines); line_number++) {      
-        let _previous_line = 0
-        let community_list = data.community_list
-        for (let _line in community_list) {     
-        let _line_coordinate = screen_lines[_line]
-          for (let _oscillator in community_list[_line]) {
-            
-            console.log("screen line: ", screen_lines[_line])
-            console.log(_line_coordinate - _previous_line)
-            let randomX = _previous_line + (rand_normal()*(_line_coordinate - _previous_line))  // random between sertain screen_lines
-            let randomY = (rand_normal()*window.innerHeight)  // random between sertain screen_lines
-            
-            let newLocation = {x: randomX, y: randomY}
-            locations.push(newLocation)
-          }
+    
+    function divide_screen(){
+      // dividing screen acording to group size
+      // var screen_lines = []  // at the constants
+      let vertical_line = 0
+      screen_lines = []
+      for (let i=0; i < group_number; i++) {
+        let community_size = _.size(data.community_list[i])
+        vertical_line += window.innerWidth * (community_size / oscillators_number) 
+        screen_lines.push( vertical_line )
+      }
+      //console.log("screen lines:", screen_lines)
+    }
+
+
+    function calculate_osc_locations() {
+      // return locations
+      let _locations = []
+      function rand_normal() {
+        // random normal distribution [0 ; 1]
+        var u = 0, v = 0;
+        while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+        while(v === 0) v = Math.random();
+        let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+        num = num / 10.0 + 0.5; // Translate to 0 -> 1
+        if (num > 1 || num < 0) return rand_normal(); // resample between 0 and 1
+        return num;
+      }
+
+      let _previous_line = 0
+      let community_list = data.community_list
+
+      for (let _line in community_list) {     
+      let _line_coordinate = screen_lines[_line]
+        for (let _oscillator in community_list[_line]) {
+
+          let randomX = _previous_line + (rand_normal()*(_line_coordinate - _previous_line))  // random between sertain screen_lines
+          let randomY = (rand_normal()*window.innerHeight)  // random between sertain screen_lines
+          
+          let newLocation = {x: randomX, y: randomY}
+          _locations.push(newLocation)
+        }
         _previous_line += _line_coordinate
       }  
+      return _locations
     }
 
-    calculate_oscillators()
+
+    fetch_data()
+    define_data_params()
+    divide_screen()
+    setLocations(calculate_osc_locations())
+    //console.log(locations)
+  }
+
+  function handlerStartEvaluation(){
+    let color_list = []
+    for (let location in locations) {
+      let color = data.phase_vector[0][location]*100
+      let hsl_color = `hsl(${color},100%,50%)`
+      color_list.push(hsl_color)
+    }
+    setColorList([color_list])
+    console.log(color_list )
+    console.log(colorList)
+  }
+
+  function handlerStopEvaluation(){
+
+  }
+
+  function handlerChangeEvaluationSpeed(){
+
   }
   // render
   return (
@@ -172,6 +239,8 @@ function App() {
         <button onClick={handleClear}>Clear</button>
         <button onClick={handleUndo}>Undo</button>
         <button onClick={handleReloadOscillatorsData}>Reload</button>
+        <button onClick={handlerStartEvaluation}>Start</button>
+        <button onClick={handlerStopEvaluation}>Stop</button>
       </div>
       <canvas 
         ref={canvasRef}
